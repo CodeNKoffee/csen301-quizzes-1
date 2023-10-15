bf56fdc961e7c3788031c5272b5ef30457f97699
Hatem Soliman <hatemthedev@gmail.com>
2023-10-15 22:19:40 +0300

    V1 - Done with exact requirement

--- ComparingSequences.java
+++ ComparingSequences.java
@@ -2,6 +2,7 @@ public class ComparingSequences {
   public static boolean containsSequence(ArrayStack s1, ArrayStack s2) {
     int size = s2.size();
     int count = 0;
+    ArrayStack dump = new ArrayStack(s1.size());
     ArrayStack tmp = new ArrayStack(size);
     while (!s2.isEmpty() && !s1.isEmpty()) {
       if (s1.top() != s2.top()) {
@@ -11,13 +12,35 @@ public class ComparingSequences {
           }
           count = 0;
         }
-        else s1.pop();
+        else dump.push(s1.pop());
       }
       else if (s1.top() == s2.top()) {
-        s1.pop();
+        dump.push(s1.pop());
         tmp.push(s2.pop());
         count++;
         if (count == size) {
+          System.out.println("S1 before restoration:");
+          s1.printStack();
+          System.out.println("S2 before restoration:");
+          s2.printStack();
+          System.out.println("TMP for S2 before emptying:");
+          tmp.printStack();
+          System.out.println("DUMP for S1 before emptying:");
+          dump.printStack();
+          System.out.println(tmp.size() + " " + dump.size());
+          int tmpSize = tmp.size();
+          int dumpSize = dump.size();
+          for (int i = 0; i < tmpSize; i++) {
+            s2.push(tmp.pop());
+            System.out.println("i: " + i);
+          }
+          for (int j = 0; j < dumpSize; j++) {
+            s1.push(dump.pop());
+            System.out.println("j: " + j);
+          }
+          System.out.println("After Restoration");
+          s1.printStack();
+          s2.printStack();
           return true;
         }
       }
@@ -37,9 +60,9 @@ public class ComparingSequences {
     s1.push(2);
     s1.push(1);
     s1.push(9);
-    s2.push(1);
     s2.push(2);
     s2.push(1);
+    s2.push(2);
     System.out.println(containsSequence(s1, s2));
   }
 }
